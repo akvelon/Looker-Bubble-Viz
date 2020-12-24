514ca2cca86e66f12fdcac511e3224d723eb66e9
Askhat Bikmetov <askhat@hey.com>
2020-12-24 16:20:01 +0300

    Add constants

---
+++ visuals/bubble-chart/src/constants/FontFamilyOptions.ts
@@ -0,0 +1,6 @@
+export const FONT_FAMILY_OPTS: Looker.OptionValue[] = [
+	{ "System UI": "system-ui" },
+	{ Serif: "serif" },
+	{ "Sans Serif": "sans-serif" },
+	{ Cursive: "cursive" },
+];
---
+++ visuals/bubble-chart/src/constants/SizeOptions.ts
@@ -0,0 +1,7 @@
+import { Size } from "../enumerables";
+
+export const SIZE_OPTS: Looker.OptionValue[] = [
+	{ Large: Size.LARGE },
+	{ Medium: Size.MEDIUM },
+	{ Small: Size.SMALL },
+];
